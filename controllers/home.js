8a160dffacaa5c056ea58d755980b779c5b5e9ce
Luigi <luigiaubrypouget@gmail.com>
2023-07-06 15:19:49 +0200

    Exo8 - design pattern MVC

---
+++ controllers/home.js
@@ -0,0 +1,4 @@
+exports.getHome = (req, res, next) => {
+    res.status(200).send('<h1>Hello World</h1>');
+  };
+  
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
+const homeRoutes = require('./routes/home');
+
 const app = express();
 
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello World</h1>');
-  });
+// app.use('/', (req, res, next) => {
+//     res.send('<h1>Hello World</h1>');
+//   });
+
+app.use('/', homeRoutes);
 
 app.listen(3000)
